9dbddb1a60ef5c2249c68842b4148faaf462138f
your Name <mohamedabdulayubm@gmail.com>
2022-06-19 22:12:05 +0530

    Perfect

--- Day_6/Program.cs
+++ Day_6/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Welcome to Day_6");
+﻿
+
+
+int a = 2;
+while (a < 10)
+{
+    double b = (Math.Pow(2, a - 1));
+    double c = (Math.Pow(2, a) - 1);
+    double d = b * c;
+    Console.WriteLine(d);
+    a++;
+    if ( d%a!=0)
+    {
+        Console.WriteLine(d+"  : It is Perfect Number");
+      
+    }
+}
+

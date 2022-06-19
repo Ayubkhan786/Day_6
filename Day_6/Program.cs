567ac108e36f8dbba1e8fef95da3ed653238f1e3
your Name <mohamedabdulayubm@gmail.com>
2022-06-19 23:18:05 +0530

    Prime

--- Day_6/Program.cs
+++ Day_6/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Welcome to Day_6");
+﻿
+int p = Convert.ToInt16(Console.ReadLine());
+int n = 0;
+for (int i = 1; i <= p; i++)
+{
+    if (p % i == 0)
+    {
+        n++;
+    }
+}
+if (n == 2)
+{
+    Console.WriteLine(" It is a Prime Number");
+}
+else
+{
+    Console.WriteLine("It is Not a Prime Number");
+}

ecabc43a0af6b7a5f9bbba2bb5b58bf99303d689
your Name <mohamedabdulayubm@gmail.com>
2022-06-19 12:10:31 +0530

    Fibonnaci

--- Day_6/Program.cs
+++ Day_6/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Welcome to Day_6");
+﻿int num1 = 0,
+num2 = 1,
+num3,
+i,
+myNum = 10;
+
+Console.WriteLine("The fibonacci series are : ");
+
+for(i = 0; i < myNum; ++i)
+{
+    num3=num1 + num2;
+    Console.WriteLine(num3);
+    num1 = num2;
+    num2 = num3;
+}

d58d5502f99b047c7a7fd76850d8ac50a2578a5d
your Name <mohamedabdulayubm@gmail.com>
2022-06-20 19:24:19 +0530

    Sqrt

--- Day_6/Program.cs
+++ Day_6/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Welcome to Day_6");
+﻿using System;
+
+namespace Day_6
+{
+    public class Newton
+    {
+
+        public void sqrt()
+        {
+
+            Console.WriteLine("Newton's method SquareRoot ");
+
+
+            double c = Convert.ToInt32(Console.ReadLine());
+            double t = c;
+            Console.WriteLine("The Initial value of t : " + t);
+
+            double Avg = c / t;
+
+            if (Math.Abs(t - c / t) > 1e-15 * t)
+            {
+                t = (t + Avg) / 2;
+            }
+
+            Console.WriteLine("The Desired value of t :" + t);
+
+        }
+        static void Main(string[] args)
+        {
+            Newton S = new Newton();
+            S.sqrt();
+        }
+    }
+}

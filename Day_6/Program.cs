30e61cd413dbfedce9bf1f5d93f4a1ba95a23b76
your Name <mohamedabdulayubm@gmail.com>
2022-06-20 12:17:18 +0530

    none

--- Day_6/Program.cs
+++ Day_6/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Welcome to Day_6");
+﻿ static void Functions()
+{
+    Random random = new Random();
+}
+
+
+int[] coupon = new int[8] { 23, 56, 78, 54, 32, 1, 7, 8 };
+int n;
+for(int i = 0; i < 8; i++)
+{
+    Console.WriteLine(Functions); 
+}

553eb1ac7fc18560cf5537e9e1b4899a19dace5f
your Name <mohamedabdulayubm@gmail.com>
2022-06-20 11:24:18 +0530

    Reverse

--- Day_6/Program.cs
+++ Day_6/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Welcome to Day_6");
+﻿Console.WriteLine("Reversing a Number");
+int n = 50, calrem,mulvar,addrem,divnum;
+for(int i = 1; i < n; i++)
+{
+    calrem = n % i;
+    mulvar = calrem * 10;
+    addrem = calrem + mulvar;
+    divnum = addrem / 10;
+    Console.WriteLine(divnum);
+}

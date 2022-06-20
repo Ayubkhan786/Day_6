93b09ef95837e4ec4bcaf1e374d53fd4e0e4ddec
your Name <mohamedabdulayubm@gmail.com>
2022-06-20 17:56:44 +0530

    stopwatch

--- Day_6/Program.cs
+++ Day_6/Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Welcome to Day_6");
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+
+Console.WriteLine("Give a Starting value");
+double start = Convert.ToDouble(Console.ReadLine());
+Stopwatch stopWatch = new Stopwatch();
+stopWatch.Start();
+
+Console.WriteLine("Give an Ending value");
+double stop = Convert.ToDouble(Console.ReadLine());
+stopWatch.Stop();
+
+TimeSpan T = stopWatch.Elapsed;
+
+
+Console.WriteLine("Time Taken to Print the End value :" + T);
+
+
+
+
+
+
+
+
+
+
+
+
+
+

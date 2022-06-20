c914f7b4ddd9c6f15cf3c79fb877f7271d23e657
your Name <mohamedabdulayubm@gmail.com>
2022-06-20 18:35:09 +0530

    payment

--- Day_6/Program.cs
+++ Day_6/Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine("Welcome to Day_6");
+﻿using System;
+
+namespace Day_6
+{
+    public class EMI
+    {
+
+        public void MonthlyPayment()
+        {
+
+            Console.WriteLine("Monthly Payment");
+
+
+            Console.WriteLine("Principal Amount");
+
+            double P = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Years");
+
+            double Y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Rate of Interest");
+
+            double R = Convert.ToInt32(Console.ReadLine());
+
+
+            double n = 12 * Y;
+            double r = R / (12 * 100);
+
+            double Payment = ((P * r) / (1 - (Math.Pow((1 + r), (-n)))));
+
+            Console.WriteLine(Payment);
+        }
+        static void Main(string[] args)
+        {
+            EMI Payment = new EMI();
+            Payment.MonthlyPayment();
+        }
+    }
+}

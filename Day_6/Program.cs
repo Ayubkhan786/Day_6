ff052dac9c9ae6bf251fa3122db1aa4eec258a52
your Name <mohamedabdulayubm@gmail.com>
2022-06-20 21:46:06 +0530

    binary

--- Day_6/Program.cs
+++ Day_6/Program.cs
@@ -1,2 +1,23 @@
-﻿Console.WriteLine("Welcome to master");
+﻿
+using System;
+public class ToBinary
+{
+    public static void Main(string[] args)
+    {
+        int n, i;
+        int[] a = new int[100];
+        Console.Write("Type the Number :=> ");
+        n = Convert.ToInt32(Console.ReadLine());
+        for (i = 0; n > 0; i++)
+        {
+            a[i] = n % 2;
+            n = n / 2;
 
+            Console.Write(a[i]);
+        }
+    }
+}
+    
+ 
+
+ 
